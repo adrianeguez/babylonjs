29b2a11b16e677f87d94d2b79c6264fad0b86e8d
Adrian Eguez <eadepto@hotmail.com>
2019-03-09 19:28:27 -0500

    web view permiso de archivos e imprimir archivos

--- WebView/src/app/search/search.component.ts
+++ WebView/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import * as fs from 'tns-core-modules/file-system';
 
 @Component({
     selector: "Search",
@@ -6,11 +7,42 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: "./search.component.html"
 })
 export class SearchComponent implements OnInit {
+    listArray;
+
     constructor() {
         // Use the constructor to inject services.
     }
 
     ngOnInit(): void {
-        // Use the "ngOnInit" handler to initialize data for the view.
+        const androidDownloadsPath = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOWNLOADS).toString();
+        console.log(androidDownloadsPath);
+
+        
+        const folderPath = androidDownloadsPath;
+
+        let internalFolder = fs.Folder.fromPath(folderPath);
+        console.log(fs.Folder.exists(internalFolder.path));
+
+        console.log(internalFolder);
+        internalFolder
+            .getEntities()
+            .then((entities) => {
+                console.log(entities);
+                // entities is array with the document's files and folders.
+                // entities.forEach((entity) => {
+                //     let  fileSize  =  fs.File.fromPath(entity.path).size;
+                //     this.listArray.push({
+                //         name: entity.name,
+                //         path: entity.path,
+                //         lastModified: entity.lastModified.toString(),
+                //         size : fileSize
+                //     });
+                // });
+                // console.log('LIST ARRAY:', this.listArray)
+            }).catch((err) => {
+            // Failed to obtain folder's contents.
+            console.log('ERROR: ', err);
+            console.log(err.stack);
+        });
     }
 }
